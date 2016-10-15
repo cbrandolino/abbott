631346653f5b7551560d6c61521941780b551faf
Claudio Brandolino <cbrandolino@gmail.com>
2016-10-15 06:11:41 +0200

    Implemented meta facilities for point

--- src/Point.js
+++ src/Point.js
@@ -1,3 +1,41 @@
-const myConstant = 1;
+const defaultDimensions = {
+  x: (it) => it,
+  y: (it) => it,
+  z: (it) => it,
+}
 
-export default myConstant;
+class Point {
+
+  constructor(
+    id, payload, dimensions, 
+    { merge=true, formatters={} } = {}
+  ) {
+    this.prepareDimensions(dimensions, merge);
+    this.meta = { 
+      id,
+      payload,
+      formatters,
+      dimensions: Object.keys(this.dimensions),
+    };
+  }
+
+  get id() {
+    return this.meta.id;
+  }
+
+  get payload() {
+    return Object.assign({}, this.meta.payload);
+  }
+
+  get dimensionKeys() {
+    return this.meta.dimensions.slice();
+  }
+
+  prepareDimensions(dimensions, merge) {
+    this.dimensions = merge ? 
+      Object.assign(defaultDimensions, dimensions) :
+      dimensions;
+  }
+}
+
+export default Point;
--- src/Point.spec.js
+++ src/Point.spec.js
@@ -1,9 +1,39 @@
+/* global describe:true it:true */
 /* eslint-disable prefer-arrow-callback */
+
 import { expect } from 'chai';
 import Point from './Point';
 
+const orderlyPayload = { x: 2, y: 1, z: 2 };
+const dimensionKeys = [ 'x', 'y', 'z' ];
+const fancyPayload = { halfX: 1, doubleY: 2 };
+const mixedPayload = { halfX: 1, y: 2 };
+const id = "Pointy McPointface";
+
 describe('Types: Point', function() {
-  it('exists', function() {
-    expect(Point).equal(1);
+  describe('Meta', function() {
+    const p = new Point(id, orderlyPayload);
+    it('gets copy of payload', function() {
+      expect(p.payload).to.eql(orderlyPayload);
+      const wontChangeProp = p.payload;
+      wontChangeProp.x = null;
+      expect(p.payload).to.eql(orderlyPayload);
+    });
+    it('gets copy of dimensionsKeys ', function() {
+      expect(p.dimensionKeys).to.eql(dimensionKeys);
+      const wontChangeProp = p.payload;
+      wontChangeProp.x = null;
+      expect(p.dimensionKeys).to.eql(dimensionKeys);
+    });
+    it('gets id', function() {
+      expect(p.id).to.eql(id);
+    });
+  })
+
+  describe('Dimensions', function() {
+    const p = new Point(id, orderlyPayload);
+    describe('Default dimensions', function() {
+    });
   });
 });
+
