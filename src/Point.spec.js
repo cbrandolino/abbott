895ec1899527ff0c96b19a6891c47890ee5401e5
Claudio Brandolino <cbrandolino@gmail.com>
2016-10-15 09:30:19 +0200

    Covered edge cases for dimensions getter

--- src/Point.spec.js
+++ src/Point.spec.js
@@ -1,10 +1,12 @@
 /* global describe:true it:true */
 /* eslint-disable prefer-arrow-callback */
+/* eslint-disable max-nested-callbacks */
+/* eslint-disable no-unused-expressions */
 
 import { expect } from 'chai';
 import Point from './Point';
 
-const orderlyPayload = { x: 2, y: 1, z: 2 };
+const orderlyPayload = { x: 2, z: 2 };
 const dimensionKeys = [ 'x', 'y', 'z' ];
 const fancyPayload = { halfX: 1, doubleY: 2 };
 const mixedPayload = { halfX: 1, y: 2 };
@@ -25,11 +27,18 @@ describe('Types: Point', function() {
     });
   });
 
-  describe('Dimensions', function() {
+  describe('Dimension getters', function() {
     describe('Default dimensions', function() {
       const p = new Point(id, orderlyPayload);
       it('exposes default dimensions', function() {
         expect(p.x).to.equal(2);
+        expect(p.z).to.equal(2);
+      });
+      it('returns undefined if dimension is not present in payload', function() {
+        expect(p.y).to.be.undefined;
+      });
+      it('throws when trying to access prop not present in dimensions', function() {
+        expect(p.w).to.throw;
       });
     });
   });
