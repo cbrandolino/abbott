0bed968afe5d130925b0d68ef74aa2819e23c6b6
Claudio Brandolino <cbrandolino@gmail.com>
2016-10-16 08:58:07 +0200

    Create immutable OrderedMap for points in series

--- src/Series.js
+++ src/Series.js
@@ -3,10 +3,14 @@ import Point from './Point'
 
 class Series {
   constructor(
-    { payloads, dimensions, pointOptions={} },
+    { payloads, dimensions={}, pointOptions={} },
     { keyDimension="x", id=null }
   ) {
-    const points = payloads.forEach(() => new Point)
+    const points = payloads.map((p, i) =>
+      new Point(p, dimensions, Object.assign({ id: i }, pointOptions)));
+    this.data = new OrderedMap(points.map(p => 
+      [ p[keyDimension], p ]));
+
   }
 }
 
--- src/Series.spec.js
+++ src/Series.spec.js
@@ -2,6 +2,35 @@
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable max-nested-callbacks */
 /* eslint-disable no-unused-expressions */
-
+import { OrderedMap } from 'immutable';
 import { expect } from 'chai';
 import Series from './Series';
+
+const pointPayloads = [
+  { bucket: 3, value: 3 },
+  { bucket: 5, value: 5 },
+];
+
+const pointDimensions = {
+  x: 'bucket',
+  y: 'value',
+}
+
+const pointOptions = {
+  id: 10,
+}
+
+const pointsObj = {
+  payloads: pointPayloads,
+  dimensions: pointDimensions,
+}
+
+describe('Types: Series', function() {
+
+  describe('Initialize: data', function() {
+    const s = new Series(pointsObj, {});
+    it('data is initialized as an OrderedMap of points', function() {
+      expect(s.data).to.be.instanceOf(OrderedMap);
+    });
+  });
+});
